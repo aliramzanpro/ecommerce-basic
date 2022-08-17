4883dc8f205b63691c957dc895010514bbba60a9
Ali Ramzan <aramzan.pro@gmail.com>
2022-08-17 17:56:58 +0200

    Entity Carrier , Order and Order Details added , and their CRUD also

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            DateTimeField::new('createdAt', 'Passée le'),
+            TextField::new('user.fullname', 'Utilisateur'),
+            TextEditorField::new('delivery', 'Adresse de livraison')->onlyOnDetail(),
+            MoneyField::new('total', 'Total produit')->setCurrency('EUR'),
+            BooleanField::new('isPaid','Payée'),
+        ];
+
+    }
+    
+}
